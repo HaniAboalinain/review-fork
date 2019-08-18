abd18ccf5d69c9ee9526ca6496d12c1ac428e9ab
haniAboalinain <Hani.aboalinain@gmail.com>
2019-08-18 11:02:49 +0300

    HANI

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 4000; app.use(cors());
 app.use(bodyParser.json()); app.listen(PORT, function () {
-    console.log("Server is running on Port: " + PORT);
+    console.log("Hani: " + PORT);
 });
